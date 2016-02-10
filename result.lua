6f00065a0bfb9b34dda777710166720f62353549
Shawn Chen <xc906@nyu.edu>
2016-02-10 17:41:08 -0500

    add description

--- result.lua
+++ result.lua
@@ -1,9 +1,10 @@
--- result scripts for assignments 1
+-- result scripts for assignments 1 (XuanangChen, WenliangZhao, 10 Feb 2016)
 require 'torch'
 require 'nn'
 require 'xlua'
 
 tsize = 10000
+--here is the set for test file
 data_path = 'mnist.t7'
 test_file = paths.concat(data_path, 'test_32x32.t7')
 result_model = torch.load('model.net')
@@ -13,7 +14,7 @@ testData = {
     labels = loaded.labels,
     size = function() return tsize end
 }
-
+-- Regularization of test data 
 testData.data = testData.data:float()
 mean = testData.data[{ {},1,{},{} }]:mean()
 std = testData.data[{ {},1,{},{} }]:std()
@@ -23,16 +24,16 @@ normalization = nn.SpatialContrastiveNormalization(1, neighborhood, 1):float()
 for i = 1,testData:size() do
    testData.data[{ i,{1},{},{} }] = normalization:forward(testData.data[{ i,{1},{},{} }])
 end
+-- writing the prediction to prediction.csv
 f = io.open('prediction.csv', 'w')
 f:write('Id' .. ',' .. 'Prediction' .. '\n')
-for t = 1, testData:size() / 500 do
+--for each testdata, go through the whole model and get the predict value 
+for t = 1, testData:size() do
     xlua.progress(t, testData:size())
     local input = testData.data[t]
     input = input:double()
     pred = result_model:forward(input)
     local _,i = torch.max(pred,1)
-    --print(pred)
-    --print(t ..' ' .. i:select(1,1) .. ' '.. testData.labels[t])
     f:write(t .. ',' .. i:select(1,1) .. '\n')
 end
 f:close()
