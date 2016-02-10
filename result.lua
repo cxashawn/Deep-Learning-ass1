9578fbfc777a004995994a9bbfae6fd67af0d494
Shawn Chen <xc906@nyu.edu>
2016-02-10 13:04:32 -0500

    update final prediction scripts

--- result.lua
+++ result.lua
@@ -1,6 +1,7 @@
 -- result scripts for assignments 1
 require 'torch'
 require 'nn'
+require 'xlua'
 
 tsize = 10000
 data_path = 'mnist.t7'
@@ -12,15 +13,26 @@ testData = {
     labels = loaded.labels,
     size = function() return tsize end
 }
+
+testData.data = testData.data:float()
+mean = testData.data[{ {},1,{},{} }]:mean()
+std = testData.data[{ {},1,{},{} }]:std()
+testData.data[{ {},1,{},{} }]:add(-mean)
+testData.data[{ {},1,{},{} }]:div(std)
+normalization = nn.SpatialContrastiveNormalization(1, neighborhood, 1):float()
+for i = 1,testData:size() do
+   testData.data[{ i,{1},{},{} }] = normalization:forward(testData.data[{ i,{1},{},{} }])
+end
+f = io.open('prediction.csv', 'w')
+f:write('Id' .. ',' .. 'Prediction' .. '\n')
 for t = 1, testData:size() / 500 do
+    xlua.progress(t, testData:size())
     local input = testData.data[t]
     input = input:double()
     pred = result_model:forward(input)
-    local max = torch.max(pred)
-    local j
-    for i = 1, 10 do 
-        if pred[i] == max then j = i
-        end
-    end
-    print(t ..' ' .. j .. ' '.. testData.labels[t])
+    local _,i = torch.max(pred,1)
+    --print(pred)
+    --print(t ..' ' .. i:select(1,1) .. ' '.. testData.labels[t])
+    f:write(t .. ',' .. i:select(1,1) .. '\n')
 end
+f:close()
